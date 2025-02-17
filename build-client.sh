03db4532b90825b58b9d598eeb8f7c730f2d5583
Justin Shreve <justin@automattic.com>
2025-02-17 08:53:37 -0500

    Add optimized UI deployment with client build script

---
+++ build-client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e  # Exit on any error
+
+echo "Starting client build at $(date)"
+cd /home/ember/LibreChat
+
+echo "Building client in temporary container..."
+docker run --rm \
+  -v $(pwd)/client:/app \
+  -w /app \
+  node:20-alpine \
+  sh -c "npm ci && npm run build"
+
+echo "Deployment completed at $(date)"
