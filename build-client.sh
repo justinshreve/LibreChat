9f1f3904fca009f11f2c503585bb952f13f51bc3
Justin Shreve <justin@automattic.com>
2025-02-17 09:13:53 -0500

    Switch to npm install in build script

--- build-client.sh
+++ build-client.sh
@@ -9,6 +9,6 @@ docker run --rm \
   -v $(pwd)/client:/app \
   -w /app \
   node:20-alpine \
-  sh -c "npm ci && npm run build"
+  sh -c "npm install && npm run build"
 
 echo "Deployment completed at $(date)"
